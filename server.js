5344b7a577a37ac213ddd6f3b7f1e94324c6c393
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-11-01 22:09:07 -0400

    refactored components to reducers.test.js

--- server.js
+++ server.js
@@ -1,58 +1,147 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useQuery } from '@apollo/react-hooks';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+// import actions
+import {
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    CLEAR_CART,
+    REMOVE_FROM_CART,
+    TOGGLE_CART,
+    UPDATE_CART_QUANTITY,
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from '../utils/actions';
+import { reducer } from '../utils/reducers';
 
-function CategoryMenu() {
-    const dispatch = useDispatch();
-    const categories = useSelector(state => state.categories);
-    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
-
-    useEffect(() => {
-        if (categoryData) {
-            // execute dispatch function with action object indicating the type of action and data to set for category state
-            dispatch({ 
-                type: UPDATE_CATEGORIES,
-                categories: categoryData.categories
-            });
-
-            categoryData.categories.forEach(category => { 
-                idbPromise('categories', 'put', category);
-            });
-        } else if (!loading) {
-            idbPromise('categories', 'get').then(categories => {
-                dispatch({ 
-                    type: UPDATE_CATEGORIES,
-                    categories: categories
-                });
-            });
+// create a sample of initial state
+const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+    cart: [
+        {
+            _id: '1',
+            name: 'Soup',
+            purchaseQuantity: 1
+        },
+        { 
+            _id: '2',
+            name: 'Bread',
+            purchaseQuantity: 2
         }
-    }, [categoryData, loading, dispatch]);
+    ], 
+    cartOpen: false
+};
 
-    const handleClick = id => {
-        dispatch({
-            type: UPDATE_CURRENT_CATEGORY,
-            currentCategory: id
-        });
-    };
+test('ADD_TO_CART', () => {
+    let newState = reducer(initialState, {
+      type: ADD_TO_CART,
+      product: { purchaseQuantity: 1 }
+    });
+  
+    expect(newState.cart.length).toBe(3);
+    expect(initialState.cart.length).toBe(2);
+});
 
-    return (
-        <div>
-            <h2>Choose a Category:</h2>
-                {categories.map(item => (
-                    <button
-                        key={item._id}
-                        onClick={() => {
-                            handleClick(item._id);
-                        }}
-                    >
-                        {item.name}
-                    </button>
-            ))}
-        </div>
-    );
-}
+test('ADD_MULTIPLE_TO_CART', () => {
+    let newState = reducer(initialState, {
+        type: ADD_MULTIPLE_TO_CART,
+        products: [{ }, { }]
+    });
 
-export default CategoryMenu;
+    expect(newState.cart.length).toBe(4);
+    expect(initialState.cart.length).toBe(2);
+});
+
+test('CLEAR_CART', () => {
+    let newState = reducer(initialState, {
+        type: CLEAR_CART
+    });
+
+    expect(newState.cartOpen).toBe(false);
+    expect(newState.cart.length).toBe(0);
+    expect(initialState.cart.length).toBe(2);
+});
+
+test('REMOVE_FROM_CART', () => {
+    let newState1 = reducer(initialState, {
+        type: REMOVE_FROM_CART,
+        _id: '1'
+    });
+
+    // cart is still open
+    expect(newState1.cartOpen).toBe(true);
+
+    // second item should now be the first
+    expect(newState1.cart.length).toBe(1);
+    expect(newState1.cart[0]._id).toBe('2');
+
+    let newState2 = reducer(newState1, {
+        type: REMOVE_FROM_CART,
+        _id: '2'
+    });
+
+    // cart is empty and closed
+    expect(newState2.cartOpen).toBe(false);
+    expect(newState2.cart.length).toBe(0);
+
+    expect(initialState.cart.length).toBe(2);
+});
+
+test('TOGGLE_CART', () => {
+    let newState = reducer(initialState, {
+        type: TOGGLE_CART
+    });
+
+    expect(newState.cartOpen).toBe(true);
+    expect(initialState.cartOpen).toBe(false);
+
+    let newState2 = reducer(newState, {
+        type: TOGGLE_CART
+    });
+
+    expect(newState2.cartOpen).toBe(false);
+});
+
+test('UPDATE_CART_QUANTITY', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CART_QUANTITY,
+        _id: '1', 
+        purchaseQuantity: 3
+    });
+
+    expect(newState.cartOpen).toBe(true);
+    expect(newState.cart[0].purchaseQuantity).toBe(3);
+    expect(newState.cart[1].purchaseQuantity).toBe(2);
+
+    expect(initialState.cartOpen).toBe(false);
+});
+
+test('UPDATE_PRODUCTS', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_PRODUCTS,
+        products: [{}, {}]
+    });
+
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+});
+
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES, 
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
+
+test('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2'
+    });
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+});
